814284f1e55e06cd591d91da8cd73390d5855ab1
Steven <muhammad95@dgu.ac.kr>
2025-09-19 18:57:28 +0900

    REJA

--- server.js
+++ server.js
@@ -1,33 +1,104 @@
+// Task 2 
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 
-// 1
 
+// 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// 2: session
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
 
-// 3
-
-app.set("views", "wievs");
-app.set("views engine", "ejs");
-
-// 4
-
-app.get("/hello", function(req, res){
-    res.end(`<h1 style="background:red">HELLO WORLD = SALOM DUNYO</h1>`);
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
 });
 
-app.get("/gift", function(req, res){
-    res.end(`<h1 style="background:red">Siz sovgalar bolimidasiz</h1>`);
+app.get("/", function (req, res) {
+    res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`the server is running successfully on port ${PORT}`);
+server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Task 1
+
+// console.log("Web Serverni boshlash");
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+
+// // 1
+
+// app.use(express.static("public"));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+// // 2: session
+
+// // 3
+
+// app.set("views", "wievs");
+// app.set("views engine", "ejs");
+
+// // 4
+
+// app.get("/hello", function(req, res){
+//     res.end(`<h1 style="background:red">HELLO WORLD = SALOM DUNYO</h1>`);
+// });
+
+// app.get("/gift", function(req, res){
+//     res.end(`<h1 style="background:red">Siz sovgalar bolimidasiz</h1>`);
+// });
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, function(){
+//     console.log(`the server is running successfully on port ${PORT}`);
+// });
