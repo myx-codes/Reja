73ef38c678ae853af94ba76650921ae1f0af50e0
Steven <muhammad95@dgu.ac.kr>
2025-09-22 16:16:07 +0900

    feat: train hamda async functions

--- server.js
+++ server.js
@@ -1,52 +1,48 @@
-// Task 2 
-console.log("Web Serverni boshlash");
+// Kutubxonalarni chaqirish
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
+// `user.json` faylidan ma'lumotlarni o'qish va JSONga o'girish
 let user;
-fs.readFile("database/user.json", "utf8", (err, data) =>{
+fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
       console.log("ERROR:", err);
     } else {
-    user = JSON.parse(data)
+    user = JSON.parse(data);
     }
- });
+});
 
-// 1: Kirish code
+// Middleware'lar (so'rovlarni qayta ishlash uchun)
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 2: Session code
-// 3: Views code
+// EJS'ni sozlash
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-// 4: Routing code
+// Yo'nalishlarni sozlash (Routing)
+app.get("/", function (req, res) {
+    res.render("harid");
+});
+
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
 });
 
 app.get('/author', (req, res) => {
-    res.render("author",{user: user});
+    res.render("author", {user: user});
 });
 
-app.get("/", function (req, res) {
-    res.render("harid");
-});
-
+// Serverni ishga tushirish
 const server = http.createServer(app);
-let PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
-
-
-
 
 
 
---
+++ train.js
@@ -0,0 +1,123 @@
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yaxshi talaba boling", // 0–20
+  "togri boshliq tanlang va koproq hato qiling", // 20–30
+  "uzingizga ishlashingizni boshlang", // 30–40
+  "siz kuchli bolgan narsalarni qiling", // 40–50
+  "yoshlarga investitsiya qiling", // 50–60
+  "endi dam oling, foydasi yoq endi", // 60
+];
+
+
+// callback function
+function maslahatBering(a, callback) {
+  if (typeof a !== "number") callback("insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setInterval(function(){
+    callback(null, list[5]);
+    }, 5000);
+  }
+}
+ console.log('passed here 0');
+maslahatBering(70, (err, data) => {
+  if (err) console.log('ERROR:', err);
+  else{
+  console.log('javob:', data);
+  }
+});
+console.log("passed here 1");
+
+
+
+
+
+
+// Async function
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number", null);
+//   else if (a <= 20) return (list[0]);
+//   else if (a > 20 && a <= 30) return(null, list[1]);
+//   else if (a > 30 && a <= 40) return(null, list[2]);
+//   else if (a > 40 && a <= 50) return(null, list[3]);
+//   else if (a > 50 && a <= 60) return(null, list[4]);
+//   else {
+//     return new Promise((resolve, reject) =>{
+//         setInterval(function(){
+//         // setTimeout(function(){
+//         resolve(list[5]);
+//     }, 1000);
+//   })
+//  }
+// }
+
+//  console.log('passed here 0')
+// maslahatBering(75)
+//   .then((data) => {
+//     console.log("javob:", data);    
+// })
+//   .catch((err) => {
+//     console.log("Error", err);
+//   })
+// console.log("passed here 1")
+
+
+// async function run () {
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     // javob = await maslahatBering(70);
+//     // console.log(javob);
+//     // javob = await maslahatBering(41);
+//     // console.log(javob);
+// }
+// run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba boling", // 0–20
+//   "togri boshliq tanlang va koproq hato qiling", // 20–30
+//   "uzingizga ishlashingizni boshlang", // 30–40
+//   "siz kuchli bolgan narsalarni qiling", // 40–50
+//   "yoshlarga investitsiya qiling", // 50–60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setTimeout(function(){
+//     callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+//  console.log('passed here 0')
+// maslahatBering(61, (err, data) => {
+//   if (err) console.log('ERROR:', err);
+//   else{
+//   console.log('javob:', data);
+//   }
+// });
+// console.log("passed here 1")
