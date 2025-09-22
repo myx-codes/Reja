a315ea1ca5eaac62da588fc07853c8549f1e9cef
Steven <muhammad95@dgu.ac.kr>
2025-09-22 18:19:02 +0900

    feat: MITASK yuklandi

--- train.js
+++ train.js
@@ -1,36 +1,53 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yaxshi talaba boling", // 0–20
-  "togri boshliq tanlang va koproq hato qiling", // 20–30
-  "uzingizga ishlashingizni boshlang", // 30–40
-  "siz kuchli bolgan narsalarni qiling", // 40–50
-  "yoshlarga investitsiya qiling", // 50–60
-  "endi dam oling, foydasi yoq endi", // 60
-];
-
-
-// callback function
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    setInterval(function(){
-    callback(null, list[5]);
-    }, 5000);
+function countLetter(letter, word) {
+  let count = 0;
+  for (let char of word) {
+    if (char === letter) {
+      count++;
+    }
   }
+  return count;
 }
- console.log('passed here 0');
-maslahatBering(70, (err, data) => {
-  if (err) console.log('ERROR:', err);
-  else{
-  console.log('javob:', data);
-  }
-});
-console.log("passed here 1");
+console.log(countLetter("i", "Alii")); // 2
+console.log(countLetter("r", "Jasurrrr")); // 4
+console.log(countLetter("T", "MIT")); // 1
+
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yaxshi talaba boling", // 0–20
+//   "togri boshliq tanlang va koproq hato qiling", // 20–30
+//   "uzingizga ishlashingizni boshlang", // 30–40
+//   "siz kuchli bolgan narsalarni qiling", // 40–50
+//   "yoshlarga investitsiya qiling", // 50–60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+
+// // callback function
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number") callback("insert a number", null);
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) callback(null, list[1]);
+//   else if (a > 30 && a <= 40) callback(null, list[2]);
+//   else if (a > 40 && a <= 50) callback(null, list[3]);
+//   else if (a > 50 && a <= 60) callback(null, list[4]);
+//   else {
+//     setInterval(function(){
+//     callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+//  console.log('passed here 0');
+// maslahatBering(70, (err, data) => {
+//   if (err) console.log('ERROR:', err);
+//   else{
+//   console.log('javob:', data);
+//   }
+// });
+// console.log("passed here 1");
 
 
 
