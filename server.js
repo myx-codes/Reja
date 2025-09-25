5217b8276c32a1f1663756932a122d2385e41b2a
Steven <muhammad95@dgu.ac.kr>
2025-09-26 02:09:24 +0900

    feat: MongoDB connected successfully

--- server.js
+++ server.js
@@ -1,113 +1,24 @@
-// Kutubxonalarni chaqirish
-console.log("WEb server boshlandi");
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-// `user.json` faylidan ma'lumotlarni o'qish va JSONga o'girish
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-      console.log("ERROR:", err);
-    } else {
-    user = JSON.parse(data);
+let db;
+const connectionString = 
+"mongodb+srv://kholbajonovm:muhammad95!@steven.k77tfwb.mongodb.net/Reja"
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},
+(err, client) => {
+    if(err) console.log("ERROR on connecting MongoDB");
+    else{
+        console.log("MongoDB connected successfully");
+        module.exports = client
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+});
     }
 });
 
-// Middleware'lar (so'rovlarni qayta ishlash uchun)
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// EJS'ni sozlash
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// Yo'nalishlarni sozlash (Routing)
-app.get("/", function (req, res) {
-    res.render("harid");
-});
-
-app.post("/create-item", (req, res) => {
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", {user: user});
-});
-
-// Serverni ishga tushirish
-const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Task 1
-
-// console.log("Web Serverni boshlash");
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-
-// // 1
-
-// app.use(express.static("public"));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-
-// // 2: session
-
-// // 3
-
-// app.set("views", "wievs");
-// app.set("views engine", "ejs");
-
-// // 4
-
-// app.get("/hello", function(req, res){
-//     res.end(`<h1 style="background:red">HELLO WORLD = SALOM DUNYO</h1>`);
-// });
-
-// app.get("/gift", function(req, res){
-//     res.end(`<h1 style="background:red">Siz sovgalar bolimidasiz</h1>`);
-// });
-
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function(){
-//     console.log(`the server is running successfully on port ${PORT}`);
-// });
